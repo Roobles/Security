2f70d08b9339164d0ed2a845989db37987e90a2a
Roobles <JohnPCooley@gmail.com>
2015-03-13 18:29:29 -0500

    Initial checkin of security database.

---
+++ sql/security_init.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS security;
+USE security;
+
+DROP TABLE IF EXISTS Exchange;
+CREATE TABLE Exchange (
+  ExchangeId SMALLINT NOT NULL AUTO_INCREMENT,
+  ExchangeName CHAR(3) NOT NULL,
+  PRIMARY KEY (ExchangeId),
+  UNIQUE (ExchangeName));
+
+DROP TABLE IF EXISTS Category;
+CREATE TABLE Category (
+  CategoryId SMALLINT NOT NULL AUTO_INCREMENT,
+  CategoryName VARCHAR(50),
+  PRIMARY KEY (CategoryId),
+  UNIQUE (CategoryName));
+
+DROP TABLE IF EXISTS Stock;
+CREATE TABLE Stock (
+ StockId INT NOT NULL AUTO_INCREMENT,
+ Ticker VARCHAR(10) NOT NULL,
+ StockName VARCHAR(50) NOT NULL,
+  ExchangeId SMALLINT NOT NULL,
+  CategoryId SMALLINT NOT NULL,
+  PRIMARY KEY (StockId),
+  FOREIGN KEY (ExchangeId) REFERENCES Exchange(ExchangeId),
+  FOREIGN KEY (CategoryId) REFERENCES Category(CategoryId),
+  CONSTRAINT uc_StockContents UNIQUE (Ticker,StockName));
